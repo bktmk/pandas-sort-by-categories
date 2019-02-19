ec75b7b8ca6d6c434dfa0ec16b643a677a9a9e80
bktmk <47651927+bktmk@users.noreply.github.com>
2019-02-19 16:09:55 +1100

    Update group and sort by row count.py

--- group and sort by row count.py
+++ group and sort by row count.py
@@ -1,9 +1,38 @@
-import pandas as pd 
-pd.set_option('display.max_rows', 5000)
 
-print("loading...")
-arcats = pd.read_csv(r'c:\Users\mattk\Downloads\bz2\huge category lookup.txt', sep='\t')
-#arc = arcats.groupby(['category']).size().sort_values('count')
-arc = arcats.groupby(['category']).size().sort_values(ascending=False)
 
-arc.head(5000)
+revenue_lookup.columns = ['sku','product','revenue']
+revenue_lookup.head()
+
+arcats.columns = ['sku','price','url','category']
+arcats.head()
+
+
+newtable = arcats
+newtable.head()
+
+
+df3 = newtable.merge(revenue_lookup,on=['sku'])
+
+
+
+df3['revenue'] = df3['revenue'].str.replace('$','')
+df3['revenue'] = df3['revenue'].str.replace(',','')
+df3['revenue'] = df3['revenue'].astype(float)
+
+
+df3 = df3.sort_values(by='revenue',ascending=False) 
+
+
+#df4 = df3.groupby(['category'])[["revenue"]].sum().sort_values(by='revenue',ascending=False) 
+#df5 = df4
+#df3.describe()
+df4 = df3[['category','revenue']]
+df4 = df4.groupby(['category']).sum().reset_index()
+df4 = df4.sort_values(by='revenue', ascending=False)
+new = df4['category'].str.split('>', 3, expand=True)
+df4['type'] = new[0]
+df4['c1'] = new[1]
+df4['c2'] = new[2]
+df4['c3'] = new[3]
+df4.to_clipboard(sep="\t")
+df4
